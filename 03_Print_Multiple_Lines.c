a795eea07da53737d52d16b86dfee38ad8511ef1
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-14 21:36:39 +0530

    Create 03_Print_Multiple_Lines.c

---
+++ 03_Print_Multiple_Lines.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Welcome to C Programming.\n");
+    printf("I am learning C language.\n");
+    printf("Practice makes me a better programmer.\n");
+
+    return 0;
+}
