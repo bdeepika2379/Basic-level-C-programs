55257e0fdaa0bb0204e6092704f2578671be790f
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-14 21:44:49 +0530

    Create 09_Basic_Output_Formatting.c

---
+++ 09_Basic_Output_Formatting.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("=================================\n");
+    printf("         STUDENT DETAILS         \n");
+    printf("=================================\n");
+
+    printf("Name   : Jeevitha\n");
+    printf("Age    : 18\n");
+    printf("Course : B.E. Computer Science\n");
+    printf("City   : Chennai\n");
+
+    printf("=================================\n");
+
+    return 0;
+}
