4461e8ec5eaf632945092621d64361f894029aa4
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-14 21:39:25 +0530

    Create 04_Escape_Sequences.c

---
+++ 04_Escape_Sequences.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name:\tJeevitha\n");
+    printf("College:\tABC Engineering College\n");
+    printf("Course:\tB.E. Computer Science\n");
+    printf("Message:\t\"Welcome to C Programming\"\n");
+    printf("Path:\tC:\\Program Files\\CodeBlocks\n");
+
+    return 0;
+}
