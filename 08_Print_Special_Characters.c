91035d2c8f4f3cef21aba4df5efd496798c9d6ff
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-14 21:43:55 +0530

    Create 08_Print_Special_Characters.c

---
+++ 08_Print_Special_Characters.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Double Quote   : \"\n");
+    printf("Single Quote   : \'\n");
+    printf("Backslash      : \\\n");
+    printf("Question Mark  : ?\n");
+    printf("Percent Symbol : %%\n");
+
+    return 0;
+}
