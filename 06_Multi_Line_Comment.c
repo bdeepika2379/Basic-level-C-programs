b26a3c1e2f5e19be7a18a97438b4160760456300
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-14 21:42:12 +0530

    Create 06_Multi_Line_Comment.c

---
+++ 06_Multi_Line_Comment.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+        This is a multi-line comment.
+        It can be used to write
+        detailed explanations
+        or documentation in the program.
+    */
+
+    printf("This program demonstrates a multi-line comment.\n");
+
+    return 0;
+}
